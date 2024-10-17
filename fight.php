8d7d019749ce43f81e74836753213f01da4a8b9a
AzterBrew <115197249+AzterBrew@users.noreply.github.com>
2024-10-17 23:50:37 +0800

    updated page accessibility with sesh

--- fight.php
+++ fight.php
@@ -1,74 +1,74 @@
 <?php
 session_start();
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+    if (isset($_SESSION['actions'])) {
+        $selection = $_SESSION['actions'];
+        $amt = count($selection);
 
-if (isset($_SESSION['actions'])) {
-    $selection = $_SESSION['actions'];
-    $amt = count($selection);
+        // $rock = presenceCheck('Rock',$selection); 
+        // $paper = presenceCheck('Paper',$selection); 
+        // $scissors = presenceCheck('Scissors',$selection); 
+        // $hook = presenceCheck('Hook',$selection);
+        echo 'rock is ' . $rock;
+    ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Fight!</title>
+            <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        <body class="bg-fight">
+            <header id="headerfight">
+                <nav class="navbar">
+                    <h1>Fight against Computer with 
+                        <?php 
+                            $i = 0;
+                            foreach($selection as $choice){
+                                if($i < $amt-1) {
+                                    echo strtoupper($choice . ", ");
+                                } 
+                                if ($i == $amt-1){
+                                    echo 'and ' . strtoupper($choice);
+                                    break;
+                                }
+                                $i++;
 
-    $rock = presenceCheck('Rock',$selection); 
-    $paper = presenceCheck('Paper',$selection); 
-    $scissors = presenceCheck('Scissors',$selection); 
-    $hook = presenceCheck('Hook',$selection);
-    echo 'rock is ' . $rock;
-?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Fight!</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body class="bg-fight">
-        <header id="headerfight">
-            <nav class="navbar">
-                <h1>Fight against Computer with 
+                            }  
+                        ?>
+                    </h1>
+                    <a href="logout.php">Logout</a>
+                </nav>
+
+            </header>
+
+            <div id="belowheader-fight">
+                <h2>Time to pick, <?php echo ucwords($_SESSION['user_name']); ?> </h2>
+            </div>
+            <div id="choices">
+                <label> Choose what to fight against the computer : </label>
+            </div>
+
+            <form action="outcome.php" method="POST">
                     <?php 
-                        $i = 0;
-                        foreach($selection as $choice){
-                            if($i < $amt-1) {
-                                echo strtoupper($choice . ", ");
-                            } 
-                            if ($i == $amt-1){
-                                echo 'and ' . strtoupper($choice);
-                                break;
-                            }
-                            $i++;
 
-                        }  
+                    foreach ($selection as $choice){
+                        //for hidden value selection arrays
+                        echo '<input type="hidden" name="selection[]" value="' . htmlspecialchars($choice) . '">';
+                        //for generating image and selection based on previously chosen
+                        echo '<button type="submit" class="img-btn" name="action" value="' . htmlspecialchars($choice) . '">'
+                        . '<img src="./images/'. strtolower(htmlspecialchars($choice)) . '.png" alt="'. strtolower(htmlspecialchars($choice)) .'"> <p>'
+                        . htmlspecialchars($choice) .
+                        '</p> </button>';
+                    }
                     ?>
-                </h1>
-                <a href="logout.php">Logout</a>
-            </nav>
+                    <!-- <button type="submit" value="submit">Proceed</button> -->
+                    
+                </form>
 
-        </header>
-
-        <div id="belowheader-fight">
-            <h2>Time to pick, <?php echo ucwords($_SESSION['user_name']); ?> </h2>
-        </div>
-        <div id="choices">
-            <label> Choose what to fight against the computer : </label>
-        </div>
-
-        <form action="outcome.php" method="POST">
-                <?php 
-
-                foreach ($selection as $choice){
-                    //for hidden value selection arrays
-                    echo '<input type="hidden" name="selection[]" value="' . htmlspecialchars($choice) . '">';
-                    //for generating image and selection based on previously chosen
-                    echo '<button type="submit" class="img-btn" name="action" value="' . htmlspecialchars($choice) . '">'
-                    . '<img src="./images/'. strtolower(htmlspecialchars($choice)) . '.png" alt="'. strtolower(htmlspecialchars($choice)) .'"> <p>'
-                    . htmlspecialchars($choice) .
-                    '</p> </button>';
-                }
-                ?>
-                <!-- <button type="submit" value="submit">Proceed</button> -->
-                
-            </form>
-
-    </body>
-    </html>
+        </body>
+        </html>
 
 
 
@@ -80,16 +80,21 @@ if (isset($_SESSION['actions'])) {
 
 
 
-    <?php    
-}
-
-
-function presenceCheck($value, $array){
-    if (in_array($value, $array)){
-        return true;
-    } else {
-        return false;
+        <?php    
     }
 
+    // function presenceCheck($value, $array){
+    //     if (in_array($value, $array)){
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+
+    // }
+} else {
+    header("Location: index.php");
+    exit();
 }
-?>
+
+    
+    ?>
--- outcome.php
+++ outcome.php
@@ -1,27 +1,76 @@
 <?php
 session_start();
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+    if (isset($_POST['selection'])) {
+        $selection = $_POST['selection'];
 
-if (isset($_POST['selection'])) {
-    $selection = $_POST['selection'];
-
-    $compResIndex = compRandom(count($selection));
-    $comchoice = $selection[$compResIndex];
-    $userchoice = $_POST['action'];
-    $results = res($comchoice, $userchoice, $_SESSION['user_name']);
-    $userstat = " ";
-    if ($results == $_SESSION['user_name']) {
-        $userstat = "win.png";
-    } elseif ($results == 'CPU') {
-        $userstat = "lose.gif";
+        $compResIndex = compRandom(count($selection));
+        $comchoice = $selection[$compResIndex];
+        $userchoice = $_POST['action'];
+        $results = res($comchoice, $userchoice, $_SESSION['user_name']);
+        $userstat = " ";
+        if ($results == $_SESSION['user_name']) {
+            $userstat = "win.png";
+        } elseif ($results == 'CPU') {
+            $userstat = "lose.gif";
+        } else {
+            $userstat = "tie.jpg";
+        }
+        $isTie = false;
+        if ($results == 'tie') {
+            $isTie = true;
+        }
     } else {
-        $userstat = "tie.jpg";
+        echo 'no detected choice';
     }
-    $isTie = false;
-    if ($results == 'tie') {
-        $isTie = true;
-    }
+
+    
+    ?>
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Results</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body class="bg-main">
+        <header id="headermain">
+            <nav class="navbar">
+                <h1>RESULTS : 
+                    <?php 
+                    if($isTie){
+                        echo "IT'S A TIE";
+                    } else {
+                        echo ucwords($results) . ' WON!';
+                        } ?> 
+                </h1>
+                <a href="logout.php">Logout</a>
+                <a href="main.php">Back to Start</a>
+            </nav>
+        </header>
+        <div id="belowheader-main">
+            <div class="parent">
+                <div id="user" class="child">
+                    <h1><?php echo ucwords($_SESSION['user_name']); ?> chose <?php echo $userchoice ?> </h1>
+                </div>
+                <div id="cpu" class="child">
+                    <h1>CPU chose <?php echo $comchoice ?> </h1>
+                    <!-- <img src=" " alt=""> -->
+                </div>
+            </div>        
+        </div>
+        <?php 
+            echo '<img id="img-end" src="./images/' . strtolower(htmlspecialchars($userstat)) . '" alt="stat">' ; 
+        ?> 
+    </body>
+    </html>
+<?php 
 } else {
-    echo 'no detected choice';
+    header("Location: index.php");
+    exit();
 }
 
 function compRandom($max){
@@ -71,45 +120,3 @@ function res($cpu,$user, $u){
 } 
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Results</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body class="bg-main">
-    <header id="headermain">
-        <nav class="navbar">
-            <h1>RESULTS : 
-                <?php 
-                if($isTie){
-                    echo "IT'S A TIE";
-                } else {
-                    echo ucwords($results) . ' WON!';
-                    } ?> 
-            </h1>
-            <a href="logout.php">Logout</a>
-            <a href="main.php">Back to Start</a>
-        </nav>
-    </header>
-    <div id="belowheader-main">
-        <div class="parent">
-            <div id="user" class="child">
-                <h1><?php echo ucwords($_SESSION['user_name']); ?> chose <?php echo $userchoice ?> </h1>
-            </div>
-            <div id="cpu" class="child">
-                <h1>CPU chose <?php echo $comchoice ?> </h1>
-                <!-- <img src=" " alt=""> -->
-            </div>
-        </div>        
-    </div>
-    <?php 
-         echo '<img id="img-end" src="./images/' . strtolower(htmlspecialchars($userstat)) . '" alt="stat">' ; 
-    ?> 
-</body>
-</html>
-
