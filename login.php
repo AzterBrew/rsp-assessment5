70a58f74111098c6a64ce896fba5fc3eda9c1d1d
AzterBrew <115197249+AzterBrew@users.noreply.github.com>
2024-10-18 11:28:19 +0800

    no db

--- login.php
+++ login.php
@@ -14,6 +14,8 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
 $uname = verify($_POST['uname']);
 $pass = verify($_POST['password']);
+$setname = "admin";
+$setpass = "pass";
 
 if (empty($uname)) {
     header("Location: index.php?error=User Name is required");
@@ -23,20 +25,15 @@ if (empty($uname)) {
     exit();
 }
 
-$sql = "SELECT * FROM assess5 WHERE user_name = '$uname' AND password = '$pass';";
-
-$results = mysqli_query($conn, $sql);
-
 // THIS IS IF IM GONNA SHOW VALIDATION SEPARATELY 
 // TEST THIS IN THE INDEX PHP IF IT WORKS
 
 if (mysqli_num_rows($results) === 1) {
     $row = mysqli_fetch_assoc($results);
-    if ($row['user_name'] === $uname && $row['password'] === $pass) {
+    if ($setname === $uname && $setpass === $pass) {
         echo 'Logged In!';
-        $_SESSION['user_name'] = $row['user_name'];
-        $_SESSION['password'] = $row['password'];
-        $_SESSION['id'] = $row['id'];
+        $_SESSION['user_name'] = $uname;
+        $_SESSION['password'] = $pass;       
         header("Location: main.php");
         exit();
     } 
